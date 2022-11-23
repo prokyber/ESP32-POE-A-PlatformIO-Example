0e2f2f15984690f37721e4ee097ae802e6b778fc
allexok <alexok997@gmail.com>
2022-11-23 16:58:47 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+
+#include <ETH.h>
+
+#ifdef ETH_CLK_MODE
+#undef ETH_CLK_MODE
+#endif
+
+#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+#define ETH_POWER_PIN   -1
+#define ETH_TYPE        ETH_PHY_LAN8720
+#define ETH_ADDR        1
+#define ETH_MDC_PIN     23
+#define ETH_MDIO_PIN    18
+#define LED_PIN         2
+
+static bool eth_connected = false;
+
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port) {
+  Serial.print("\nconnecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("closing connection\n");
+  client.stop();
+}
+
+void setup() {
+  pinMode(LED_PIN,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("ESP32-POE-A begin\n");
+  WiFi.onEvent(WiFiEvent);
+  ETH.begin(ETH_ADDR, ETH_POWER_PIN, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_TYPE, ETH_CLK_MODE);
+}
+
+
+void loop() {
+  if (eth_connected) {
+    testClient("google.com", 80);
+  }
+  digitalWrite(LED_PIN,HIGH);
+  delay(100);
+  digitalWrite(LED_PIN,LOW);
+  delay(100);
+}
